a03c44098c03bc3abdc1e0aebb161899788f6b33
Keplyx <keplyx@gmail.com>
2018-02-17 19:30:39 +0100

    added old work control in settings

--- app/src/main/java/com/clubinfo/insat/memorisia/fragments/SettingsFragment.java
+++ app/src/main/java/com/clubinfo/insat/memorisia/fragments/SettingsFragment.java
@@ -1,25 +1,18 @@
 package com.clubinfo.insat.memorisia.fragments;
 
-import android.app.Fragment;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
+import android.preference.SwitchPreference;
 
 import com.clubinfo.insat.memorisia.R;
 
 
 public class SettingsFragment extends PreferenceFragment {
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,4 +20,5 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
+    
 }
